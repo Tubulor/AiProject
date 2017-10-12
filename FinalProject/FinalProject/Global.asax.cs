222ef09fffc43dca6cd4095703d148250d003068
Eon Torch <idanl22@gmail.com>
2017-10-12 17:34:48 +0300

    תיקנתי את הבעיה

--- FinalProject/FinalProject/Global.asax.cs
+++ FinalProject/FinalProject/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+using FinalProject.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +14,7 @@ namespace FinalProject
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<ApplicationDbContext>());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
